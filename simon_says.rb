c5eeef50dc2e3d18190e26258c30a8e15ee5fd10
curtischatch <curtis.chat.hk@gmail.com>
2018-08-29 12:29:51 -0400

    wrote all methods for testing

---
+++ simon_says.rb
@@ -0,0 +1,25 @@
+def echo(str)
+  return str
+end
+
+def shout(str)
+  return str.upcase
+end
+
+def repeat(str, num)
+  return ([str] * num).join(" ")
+end
+
+p repeat("hell0", 2)
+
+def start_of_word(str, num)
+  return str.split(//).first(num).join(" ")
+end
+
+p start_of_word("hello", 2)
+
+def first_word(str)
+  return str.split.first
+end
+
+p first_word("man this is cool")
