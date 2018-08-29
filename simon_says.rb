cb7f850fdf95c716c6de4186403962fbef6bf0ff
curtischatch <curtis.chat.hk@gmail.com>
2018-08-29 12:31:24 -0400

    removed stuff

--- simon_says.rb
+++ simon_says.rb
@@ -10,16 +10,16 @@ def repeat(str, num)
   return ([str] * num).join(" ")
 end
 
-p repeat("hell0", 2)
+# p repeat("hell0", 2)
 
 def start_of_word(str, num)
   return str.split(//).first(num).join(" ")
 end
 
-p start_of_word("hello", 2)
+# p start_of_word("hello", 2)
 
 def first_word(str)
   return str.split.first
 end
 
-p first_word("man this is cool")
+# p first_word("man this is cool")
